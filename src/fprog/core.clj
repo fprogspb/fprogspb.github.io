72a5748019f9dbcb6baf3abe52fd02d6cc75b3fe
niquola <niquola@gmail.com>
2016-01-15 09:39:24 +0300

    progress

--- src/fprog/core.clj
+++ src/fprog/core.clj
@@ -2,29 +2,27 @@
   (:require [esthatic.core :as es]))
 
 (defn menu [{data :data}]
-  [:div#menu
+  [:div
    [:$style
-    [:#menu {:border-bottom {:width [:$px 1]
-                             :style "solid"
-                             :color [:$c :gray]}}
-     [:.item {:display "inline-block"
-              :padding     [:$px 10]
-              :font-size   [:$v 2]
-              :line-height [:$v 3]}
-
-      [:.icon {:height  [:$v 3]
-               :width   [:$v 3]
-               :display "inline-block"}
-
-       [:.logo {:fill "none"
-                :stroke [:$c :red]
-                :stroke-width 3}]]]]]
-   [:a.item {:href "#"}
-    [:.icon [:$svg "icon"]]
-    [:$nbsp]
-    (get-in data [:texts :brand])]
-   (for [it (:nav data)]
-     [:a.item {:href "#"} (:title it)])])
+    [:nav.navbar
+     [:.navbar-brand
+      {:background [:$c :black]
+       :display "inline-block"
+       :color [:$c :white]
+       :font-size [:$v 3]
+       :padding {:top [:$v 5] :bottom 0 :left 0 :right 0}}]
+     [:a {:color [:$c :light-gray]
+          :font-size [:$v 1.5]
+          :margin-left [:$v 2]
+          :border-top [:$border 4 :transparent]
+          :line-height [:$v 2]}
+      [:&:hover {:background-color "transparent"
+                 :color "white" 
+                 :border-top [:$border :orange]}]]]]
+   [:$nav
+    (get-in data [:texts :brand])
+    (for [it (:nav data)]
+      [:$nav-item {:href "#"} (:title it)])]])
 
 (defn layout [{data :data :as opts} cnt]
   [:html
@@ -32,18 +30,34 @@
     [:$cdn-css :bootsrtrap]
     [:$google-font :Exo-2]
     [:$style
-     [:body {:padding [:$v 2]
+     [:body {:padding 0 
+             :background-color  [:$c :dark-gray]
+             :color "white"
              :font-family "'Exo 2'"}]]]
    [:body
-    [:.container
-     (menu opts)
-     [:$br 10]
-     cnt]]])
+    (menu opts)
+    cnt]])
 
 (defn index [{data :data :as opts}]
-  [:.wrap
-   [:h3 "Hello"]
-   [:$inspect data]])
+  [:.container
+   [:$style
+    [:.moto
+     {:position "fixed"
+      :text-align "center"
+      :color [:$c :white]
+      :font-size [:$v 2]
+      :border-top [:$border 1 :light-gray]
+      :bottom 0
+      :left 0
+      :right 0
+      :background [:$c :black]
+      :opacity 0.5
+      :padding [:$v 1]}]]
+   [:$br]
+   [:$svg "logo"]
+   [:$br]
+   [:.moto "Благородное собрание функциональных программистов"]
+   ])
 
 (def routes
   {:GET #'index})
@@ -51,8 +65,15 @@
 (def styles
   {:$v 18
    :$c {:red "red"
+        :white "white"
+        :dark-gray "rgba(46,48,58,0.96)"
+        :light-gray "#ccc"
+        :orange "#D56530"
         :gray "#777"
-        :blue "blue"}})
+        :black "black"
+        :transparent "transparent"
+        :blue "#42A3EE"}})
+
 
 (comment
   (def stop (es/start {:routes #'routes
